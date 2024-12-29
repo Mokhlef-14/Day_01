5c8f12cfa05ec2d3525f781d570a0f48a93d99e5
mohamed mokhlef <mohamedmokhlef@hotmail.com>
2024-12-29 21:04:04 +0200

    Add project files.

---
+++ Task_01/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Islam's carpet cleaning service \nCharges :\n $25 per small \n $35 per large\n sales tax rate is 6%\nEstimates are valid for 30 days  \n");
+
+            Console.WriteLine("please enter how many small carpets to wash : \n  ");
+            int numOfSmall = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter how many large carpets to wash : \n  ");
+            int numOfLarge = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"number of small carpets {numOfSmall} : \n  ");
+            Console.WriteLine($"number of small carpets {numOfLarge} : \n  ");
+
+
+            double tax = 0.06;
+            int cost = (numOfSmall * 25) + (numOfLarge * 35);
+            double total = cost + (cost * tax);
+
+            Console.WriteLine($"the cost : {cost}");
+            Console.WriteLine("tax : 6%");
+            Console.WriteLine($"the total : {total}");
+        }
+    }
+}
